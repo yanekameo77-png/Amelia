933af0ef249f79c6d32a6d321e43471405c381ec
yanekameo77-png <yanekameo77@gmail.com>
2026-05-22 14:17:29 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,7 @@ st.title("🎈 karunia app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+import streamlit as st
+
+st.title("This is a title")
+st.title("_Streamlit_ is :babyblue[juun] :purple alien:")
