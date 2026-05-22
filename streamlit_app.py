3c26bb9ff3587f16a479180502501a8ebcd387e1
yanekameo77-png <yanekameo77@gmail.com>
2026-05-22 14:19:17 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit as st
 st.title("🎈 karunia app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+)
 import streamlit as st
 
 st.title("This is a title")
