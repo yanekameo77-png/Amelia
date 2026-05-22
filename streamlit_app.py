56aafec931a03f0df83b054c3f0514b8733c05a6
yanekameo77-png <yanekameo77@gmail.com>
2026-05-22 14:10:13 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 welcome to karunia app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
